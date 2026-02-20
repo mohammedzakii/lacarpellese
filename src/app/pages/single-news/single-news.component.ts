a9088fca691e4eea77edd7c118a3c1f1f6ab3429
mohammedzakii <mohamed_mohamed9670@yahoo.com>
2026-02-20 21:23:39 +0200

    done page

--- src/app/pages/single-news/single-news.component.ts
+++ src/app/pages/single-news/single-news.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-single-news',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './single-news.component.html',
   styleUrl: './single-news.component.css'
 })
--- src/app/pages/single-product/single-product.component.ts
+++ src/app/pages/single-product/single-product.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-single-product',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './single-product.component.html',
   styleUrl: './single-product.component.css'
 })
